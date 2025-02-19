aa6e43bde2e4f270194f02872c7dd432284a2ad6
Hafizhan Amril <152627260+AKUBATU@users.noreply.github.com>
2025-02-19 20:00:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Website Portofolio telah dimuat");
 });
+
+function toggleMenu() {
+    document.querySelector("nav ul").classList.toggle("active");
+}
